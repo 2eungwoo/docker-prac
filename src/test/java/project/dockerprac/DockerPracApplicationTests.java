92b970b8d56a3888ec7d7df9b4a79d63a32d270f
swlee <swo98@naver.com>
2025-04-06 19:25:28 +0900

    project setting

---
+++ src/test/java/project/dockerprac/DockerPracApplicationTests.java
@@ -0,0 +1,13 @@
+package project.dockerprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
